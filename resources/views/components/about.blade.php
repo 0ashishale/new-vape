8871807428a767c0ba34f6c609758bf709475700
RAJESHSHREES <rajeshshrees0829@gmail.com>
2022-07-10 11:47:27 +0545

    page added

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,2 +1,17 @@
 @include('components.main.master')
-this is about page
+<div >
+    <div class="container">
+   <div class="sub-nav">
+        <a href="/">Home</a>
+        <a href="">/</a>
+        <a href="/blog">About Page</a>
+   </div>
+   <div>
+    <h1>ABOUT</h1>
+   </div>
+    <div >
+        <p>Thanks so much for visiting our online store. Our team is excited to provide you with an impeccable online shopping experience and remains available to assist at any time. If you have questions, comments or concerns about your order or the content found within this website, please feel free to contact us via telephone or email and one of our experienced team members will get back to you right away.  Again, thanks for visiting our store and we look forward to serving you in the future.</p>
+       
+    </div>
+    </div>
+</div>
---
+++ resources/views/components/blog.blade.php
@@ -0,0 +1,17 @@
+@include('components.main.master')
+<div >
+    <div class="container">
+   <div class="sub-nav">
+        <a href="/">Home</a>
+        <a href="">/</a>
+        <a href="/blog">Blog Home</a>
+   </div>
+   <div>
+    <h1>BLOG HOME</h1>
+   </div>
+    <div >
+        <p>Welcome to our online store! Our team is proud to announce that we're now open for business, and we look forward to serving you all in the future. If you have any questions about this store or the products found within, please don't hesitate to contact us any time. Our website has been carefully designed to provide you with an amazingly flexible online shopping experience, and its ease of navigation is something we think you'll grow to depend on and appreciate. Feel free to browse our entire product catalog, and let us know if you have any questions, comments or concerns about the items housed within. Our team is always ready and willing to assist our customers, and we are happy for your visit.</p>
+        <button>Read More</button>
+    </div>
+    </div>
+</div>

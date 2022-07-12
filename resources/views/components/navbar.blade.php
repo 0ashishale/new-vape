1f200ab729f5ae8c98788c5de9b46298d1faf26c
0ashishale <ashishale42@gmail.com>
2022-07-12 14:55:12 +0545

    update responsive navba

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -17,7 +17,7 @@
                          Register</a>
                          <a href=""javascript:void(0);" class="icon" onclick="myFunction()""><i class="fa-solid fa-bars" ></i></a> 
                  </div> --}}
-                 <div class="topnav" id="myTopnav">
+                 <div class="navbar__navLinks" id="mynavbar__navLinks">
                     <a href="/" class="active">Home</a>
                     <a href="/about">About</a>
                     <a href="/blog">Blog</a>
@@ -35,11 +35,11 @@
 
 <script>
     function myFunction() {
-      var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
+      var x = document.getElementById("mynavbar__navLinks");
+      if (x.className === "navbar__navLinks") {
         x.className += " responsive";
       } else {
-        x.className = "topnav";
+        x.className = "navbar__navLinks";
       }
     }
     </script>
